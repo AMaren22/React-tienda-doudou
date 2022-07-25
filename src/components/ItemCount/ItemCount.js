6c4449001d546ffa543b598e425e03e2315ba36b
Augusto <augustomarengo@hotmail.com>
2022-07-25 13:57:36 -0300

    Agrego opción de agregar productos

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+import './itemCount.css'
+
+const ItemCount = ({addProduct}) =>{
+
+    const stock = 7
+    
+   const [count, setCount] = useState(0)
+
+   const increase = () =>{
+    count<stock && setCount(count+1);
+   }
+   const decrease = () =>{
+    count>0 && setCount(count-1)
+   }
+   
+    return(
+        <div className='itemCountContainer'>
+            <div className='buttonCountContainer'>
+            <button onClick={decrease} disabled={count === 0}>
+                -
+            </button>
+            <span>{count}</span>
+            <button onClick={increase} disabled={count === stock}>
+                +
+            </button>
+            </div>
+            <button disabled={count === 0 ? true : null} onClick={() =>addProduct(count)} className='addToCart'>
+                Add to cart
+            </button>
+        </div>
+    )
+}
+
+export default ItemCount
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,18 @@
+import ItemCount from '../ItemCount/ItemCount'
+
 import './itemListContainer.css'
 
 const ItemListContainer = (props) =>{
+
+
+    const addProduct = (count) =>{
+        alert(`Agregaste ${count} producto al carrito`)
+    }
+
     return(
-        <div>
+        <div className='itemListContainer'>
             <h1>{props.greeting}</h1>
-           
-            
+            <ItemCount addProduct={addProduct}/>       
         </div>
     )
 }
